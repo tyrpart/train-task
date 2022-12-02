0ed1dda93f1fa46c68ca704e08e4640542cb66c3
Revopart <ieapold2@mail.ru>
2022-12-03 00:24:23 +0300

    1

--- task_1/src/Main.java
+++ task_1/src/Main.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Main {
 
     public static final double ABSOLUTE_ZERO = -273.15;
-    public static final String positiveAns = "Y";
+    public static final String POSITIVE_ANS = "Y";
 
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         String ans = "Y";
-        while (ans.equalsIgnoreCase(positiveAns)) {
+        while (ans.equalsIgnoreCase(POSITIVE_ANS)) {
             System.out.println("Type your celsius temperature");
             double celsiusValue = console.nextDouble();
             if (celsiusValue > ABSOLUTE_ZERO) {
